7e01913aaac5c7a5ae84b330d35b34d424e264e3
Gaston Mazzei <gastonmazzei95@gmail.com>
2020-10-21 19:26:43 -0300

    fixed clock

--- clock.py
+++ clock.py
@@ -1,11 +1,16 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-from engine import *
+from app.engine import *
+import os
 
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval',seconds=20)
 def timed_job():
+  print('RUN!')
+  cdir = os.getcwd()
+  os.chdir('app')  
   main()
+  os.chdir(cdir)
   return
 
 
