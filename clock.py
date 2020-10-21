fbd7bd9f55c9bfb6fccbca45a63a8ce835ed05db
Gaston Mazzei <gastonmazzei95@gmail.com>
2020-10-21 20:30:58 -0300

    temporary freq reduction

--- clock.py
+++ clock.py
@@ -4,7 +4,7 @@ import os
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval',seconds=10)
+@sched.scheduled_job('interval',seconds=3600)
 def timed_job():
   print('RUN!')
   cdir = os.getcwd()
