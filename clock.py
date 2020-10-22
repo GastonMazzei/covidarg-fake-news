2567aec649b21621e1e87cace32d0d5359c13180
Gaston Mazzei <gastonmazzei95@gmail.com>
2020-10-21 22:01:56 -0300

    update

--- clock.py
+++ clock.py
@@ -4,7 +4,7 @@ import os
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval',seconds=20)
+@sched.scheduled_job('interval',seconds=2000)
 def timed_job():
   print('RUN!')
   #cdir = os.getcwd()
